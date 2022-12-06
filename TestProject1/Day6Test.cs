0d57a652374fa3395b80fff52137271b33e41ed1
Dominik Fladung <dominik.fladung@gmx.de>
2022-12-06 23:26:39 +0100

    day6

--- TestProject1/Day6Test.cs
+++ TestProject1/Day6Test.cs
@@ -12,24 +12,73 @@ public class Day6Test : IDayTest
     [Test]
     public void Part1Test()
     {
-        Assert.That(GetDay().Part1(Day.GetPuzzleInput("6.txt")), Is.EqualTo(7766));
+        Assert.That(GetDay().Part1(Day.GetPuzzleInput("6.txt")), Is.EqualTo(1235));
     }
 
     [Test]
     public void Part1SampleTest()
     {
-        Assert.That(GetDay().Part1(Day.GetPuzzleInput("6_sample.txt")), Is.EqualTo(157));
+        Assert.That(GetDay().Part1(Day.GetPuzzleInput("6_sample.txt")), Is.EqualTo(7));
+    }
+    
+    [Test]
+    public void Part1SampleTest2()
+    {
+        Assert.That(GetDay().Part1(Day.GetPuzzleInput("6_sample_2.txt")), Is.EqualTo(5));
     }
 
     [Test]
+    public void Part1SampleTest3()
+    {
+        Assert.That(GetDay().Part1(Day.GetPuzzleInput("6_sample_3.txt")), Is.EqualTo(6));
+    }
+    
+    [Test]
+    public void Part1SampleTest4()
+    {
+        Assert.That(GetDay().Part1(Day.GetPuzzleInput("6_sample_4.txt")), Is.EqualTo(10));
+    }
+    
+    [Test]
+    public void Part1SampleTest5()
+    {
+        Assert.That(GetDay().Part1(Day.GetPuzzleInput("6_sample_5.txt")), Is.EqualTo(11));
+    }
+    
+    
+    [Test]
     public void Part2Test()
     {
-        Assert.That(GetDay().Part2(Day.GetPuzzleInput("6.txt")), Is.EqualTo(2415));   
+        Assert.That(GetDay().Part2(Day.GetPuzzleInput("6.txt")), Is.EqualTo(3051));   
     }
 
     [Test]
     public void Part2SampleTest()
     {
-        Assert.That(GetDay().Part2(Day.GetPuzzleInput("6_sample.txt")), Is.EqualTo(70));
+        Assert.That(GetDay().Part2(Day.GetPuzzleInput("6_sample.txt")), Is.EqualTo(19));
+    }
+    
+    [Test]
+    public void Part2SampleTest2()
+    {
+        Assert.That(GetDay().Part2(Day.GetPuzzleInput("6_sample_2.txt")), Is.EqualTo(23));
+    }
+
+    [Test]
+    public void Part2SampleTest3()
+    {
+        Assert.That(GetDay().Part2(Day.GetPuzzleInput("6_sample_3.txt")), Is.EqualTo(23));
+    }
+    
+    [Test]
+    public void Part2SampleTest4()
+    {
+        Assert.That(GetDay().Part2(Day.GetPuzzleInput("6_sample_4.txt")), Is.EqualTo(29));
+    }
+    
+    [Test]
+    public void Part2SampleTest5()
+    {
+        Assert.That(GetDay().Part2(Day.GetPuzzleInput("6_sample_5.txt")), Is.EqualTo(26));
     }
 }
--- adventOfCode2022/Day6.cs
+++ adventOfCode2022/Day6.cs
@@ -4,11 +4,38 @@ public class Day6 : Day
 {
     public override object Part1(string[] puzzleInput)
     {
-        throw new NotImplementedException();
+        var datastream = puzzleInput[0];
+        return GetMarker(datastream, 4);
     }
 
     public override object Part2(string[] puzzleInput)
     {
-        throw new NotImplementedException();
+        var datastream = puzzleInput[0];
+        return GetMarker(datastream, 14);
+    }
+
+    private static object GetMarker(string datastream, int size)
+    {
+        var buffer = new Queue<char>();
+        
+        for (var i = 0; i < datastream.Length; i++)
+        {
+            var c = datastream[i];
+            if (i < size)
+            {
+                buffer.Enqueue(c);
+            }
+            else if (buffer.Distinct().Count() == size)
+            {
+                return i;
+            }
+            else
+            {
+                buffer.Dequeue();
+                buffer.Enqueue(c);
+            }
+        }
+
+        return -1;
     }
 }
